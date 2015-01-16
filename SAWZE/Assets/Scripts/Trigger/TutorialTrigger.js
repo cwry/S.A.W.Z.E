0a8f4c9c0117ad3a3e5fc79eac6a70e9546076fb
Daniel <daniel.pochert@gmail.com>
2015-01-16 13:36:00 +0100

    Alarm Animation + Timer , Tutorial Fix

--- SAWZE/Assets/Scripts/Trigger/TutorialTrigger.js
+++ SAWZE/Assets/Scripts/Trigger/TutorialTrigger.js
@@ -1,6 +1,7 @@
 ﻿#pragma strict
 
 var minDuration : float;
+var message : GameObject;
 
 private var triggered : boolean = false;
 private var lastActivation : float;
@@ -10,13 +11,13 @@ function OnTriggerEnter2D(other: Collider2D){
 	 	Time.timeScale = 0;
 	 	lastActivation = Time.realtimeSinceStartup;
 	 	triggered = true;
-	 	transform.Find("Message").GetComponent(Canvas).enabled = true;
+	 	message.GetComponent(Canvas).enabled = true;
 	 }
 }
 
 function Update(){
 	if(triggered && Time.realtimeSinceStartup - lastActivation >= minDuration && Input.anyKey){
-		transform.Find("Message").GetComponent(Canvas).enabled = false;
+		message.GetComponent(Canvas).enabled = false;
 		Time.timeScale = 1;
 	}
 }
