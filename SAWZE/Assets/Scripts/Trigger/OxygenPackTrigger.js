22e231ac02857d39c93ccbb5ea210a62d305a479
Daniel <daniel.pochert@gmail.com>
2015-01-17 17:46:53 +0100

    Oxygen Particle implemented

--- SAWZE/Assets/Scripts/Trigger/OxygenPackTrigger.js
+++ SAWZE/Assets/Scripts/Trigger/OxygenPackTrigger.js
@@ -1,10 +1,13 @@
 ﻿#pragma strict
 
 var amount : float;
+var particle : ParticleSystem;
 
 function OnTriggerEnter2D(other: Collider2D){
 	 if(other.gameObject.name == "Player"){
 	 	other.gameObject.SendMessage("addOxygen", amount);
+	 	var par = Instantiate(particle);
+	 	par.transform.position = gameObject.transform.position;
 	 	Destroy(gameObject);
 	 }
 }
