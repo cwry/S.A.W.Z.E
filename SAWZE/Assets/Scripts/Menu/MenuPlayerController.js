6bb377fa361fd887ab0f1313db50ba7d38891a87
Daniel <daniel.pochert@gmail.com>
2015-01-22 17:25:04 +0100

    Menu!

---
+++ SAWZE/Assets/Scripts/Menu/MenuPlayerController.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var render : Renderer;
+
+function Update(){
+	transform.position.x -= Time.deltaTime * 1.5;
+	if(transform.position.x <= -3){
+		transform.position.x = 4;
+	}
+}
---
+++ SAWZE/Assets/Scripts/Menu/MenuZombieController.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var render : Renderer;
+var model : GameObject;
+
+function Awake(){
+	model.animation["Take 001"].speed = 4;
+}
+
+function Update(){
+	transform.position.x -= Time.deltaTime * 1.5;
+	if(transform.position.x < -3){
+		transform.position.x = 4;
+	}
+}
