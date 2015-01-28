e29dca358fab56e3ec1cd14b2452222a10377e48
Daniel <daniel.pochert@gmail.com>
2015-01-28 10:56:32 +0100

    Tutorial Box Fade

--- SAWZE/Assets/Scripts/Trigger/TutorialTrigger.js
+++ SAWZE/Assets/Scripts/Trigger/TutorialTrigger.js
@@ -5,6 +5,12 @@ var message : GameObject;
 
 private var triggered : boolean = false; 
 private var lastActivation : float;
+private var img : UnityEngine.UI.Image;
+
+
+function Awake(){
+	img = message.Find("MessageImage").GetComponent(UnityEngine.UI.Image);
+}
 
 function OnTriggerEnter2D(other: Collider2D){
 	 if(!triggered && other.gameObject.name == "Player"){
@@ -12,10 +18,12 @@ function OnTriggerEnter2D(other: Collider2D){
 	 	lastActivation = Time.realtimeSinceStartup; 
 	 	triggered = true;
 	 	message.GetComponent(Canvas).enabled = true;
+	 	img.color.a = 0;
 	 }
 }
 
 function Update(){
+	img.color.a = Mathf.Lerp(0, 1, (Time.realtimeSinceStartup - lastActivation) * 3);
 	if(triggered && (Time.realtimeSinceStartup - lastActivation >= minDuration) && Input.anyKey){ 
 		message.GetComponent(Canvas).enabled = false;
 		Time.timeScale = 1;
